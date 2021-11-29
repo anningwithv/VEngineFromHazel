7cd0183ef1f11ea81681a5415a06404a71413874
zwei <zwei@wooduan.com>
2021-11-29 15:15:20 +0800

    Input

---
+++ SimpleEngine/SimpleEngine/src/Platform/Windows/WindowsInput.cpp
@@ -0,0 +1,46 @@
+#include "VEnginePCH.h"
+#include "WindowsInput.h"
+
+#include "VEngine/Application.h"
+#include <GLFW/glfw3.h>
+
+namespace VEngine {
+
+	Input* Input::s_Instance = new WindowsInput();
+
+	bool WindowsInput::IsKeyPressedImpl(int keycode)
+	{
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		auto state = glfwGetKey(window, keycode);
+		return state == GLFW_PRESS || state == GLFW_REPEAT;
+	}
+
+	bool WindowsInput::IsMouseButtonPressedImpl(int button)
+	{
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		auto state = glfwGetMouseButton(window, button);
+		return state == GLFW_PRESS;
+	}
+
+	std::pair<float, float> WindowsInput::GetMousePositionImpl()
+	{
+		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+
+		return { (float)xpos, (float)ypos };
+	}
+
+	float WindowsInput::GetMouseXImpl()
+	{
+		std::pair<float, float> pair = GetMousePositionImpl();
+		return pair.first;
+	}
+
+	float WindowsInput::GetMouseYImpl()
+	{
+		std::pair<float, float> pair = GetMousePositionImpl();
+		return pair.second;
+	}
+
+}
---
+++ SimpleEngine/SimpleEngine/src/VEngine/KeyCodes.h
@@ -0,0 +1,125 @@
+#pragma once
+
+// From glfw3.h
+#define VENGINE_KEY_SPACE              32
+#define VENGINE_KEY_APOSTROPHE         39  /* ' */
+#define VENGINE_KEY_COMMA              44  /* , */
+#define VENGINE_KEY_MINUS              45  /* - */
+#define VENGINE_KEY_PERIOD             46  /* . */
+#define VENGINE_KEY_SLASH              47  /* / */
+#define VENGINE_KEY_0                  48
+#define VENGINE_KEY_1                  49
+#define VENGINE_KEY_2                  50
+#define VENGINE_KEY_3                  51
+#define VENGINE_KEY_4                  52
+#define VENGINE_KEY_5                  53
+#define VENGINE_KEY_6                  54
+#define VENGINE_KEY_7                  55
+#define VENGINE_KEY_8                  56
+#define VENGINE_KEY_9                  57
+#define VENGINE_KEY_SEMICOLON          59  /* ; */
+#define VENGINE_KEY_EQUAL              61  /* = */
+#define VENGINE_KEY_A                  65
+#define VENGINE_KEY_B                  66
+#define VENGINE_KEY_C                  67
+#define VENGINE_KEY_D                  68
+#define VENGINE_KEY_E                  69
+#define VENGINE_KEY_F                  70
+#define VENGINE_KEY_G                  71
+#define VENGINE_KEY_H                  72
+#define VENGINE_KEY_I                  73
+#define VENGINE_KEY_J                  74
+#define VENGINE_KEY_K                  75
+#define VENGINE_KEY_L                  76
+#define VENGINE_KEY_M                  77
+#define VENGINE_KEY_N                  78
+#define VENGINE_KEY_O                  79
+#define VENGINE_KEY_P                  80
+#define VENGINE_KEY_Q                  81
+#define VENGINE_KEY_R                  82
+#define VENGINE_KEY_S                  83
+#define VENGINE_KEY_T                  84
+#define VENGINE_KEY_U                  85
+#define VENGINE_KEY_V                  86
+#define VENGINE_KEY_W                  87
+#define VENGINE_KEY_X                  88
+#define VENGINE_KEY_Y                  89
+#define VENGINE_KEY_Z                  90
+#define VENGINE_KEY_LEFT_BRACKET       91  /* [ */
+#define VENGINE_KEY_BACKSLASH          92  /* \ */
+#define VENGINE_KEY_RIGHT_BRACKET      93  /* ] */
+#define VENGINE_KEY_GRAVE_ACCENT       96  /* ` */
+#define VENGINE_KEY_WORLD_1            161 /* non-US #1 */
+#define VENGINE_KEY_WORLD_2            162 /* non-US #2 */
+
+/* Function keys */
+#define VENGINE_KEY_ESCAPE             256
+#define VENGINE_KEY_ENTER              257
+#define VENGINE_KEY_TAB                258
+#define VENGINE_KEY_BACKSPACE          259
+#define VENGINE_KEY_INSERT             260
+#define VENGINE_KEY_DELETE             261
+#define VENGINE_KEY_RIGHT              262
+#define VENGINE_KEY_LEFT               263
+#define VENGINE_KEY_DOWN               264
+#define VENGINE_KEY_UP                 265
+#define VENGINE_KEY_PAGE_UP            266
+#define VENGINE_KEY_PAGE_DOWN          267
+#define VENGINE_KEY_HOME               268
+#define VENGINE_KEY_END                269
+#define VENGINE_KEY_CAPS_LOCK          280
+#define VENGINE_KEY_SCROLL_LOCK        281
+#define VENGINE_KEY_NUM_LOCK           282
+#define VENGINE_KEY_PRINT_SCREEN       283
+#define VENGINE_KEY_PAUSE              284
+#define VENGINE_KEY_F1                 290
+#define VENGINE_KEY_F2                 291
+#define VENGINE_KEY_F3                 292
+#define VENGINE_KEY_F4                 293
+#define VENGINE_KEY_F5                 294
+#define VENGINE_KEY_F6                 295
+#define VENGINE_KEY_F7                 296
+#define VENGINE_KEY_F8                 297
+#define VENGINE_KEY_F9                 298
+#define VENGINE_KEY_F10                299
+#define VENGINE_KEY_F11                300
+#define VENGINE_KEY_F12                301
+#define VENGINE_KEY_F13                302
+#define VENGINE_KEY_F14                303
+#define VENGINE_KEY_F15                304
+#define VENGINE_KEY_F16                305
+#define VENGINE_KEY_F17                306
+#define VENGINE_KEY_F18                307
+#define VENGINE_KEY_F19                308
+#define VENGINE_KEY_F20                309
+#define VENGINE_KEY_F21                310
+#define VENGINE_KEY_F22                311
+#define VENGINE_KEY_F23                312
+#define VENGINE_KEY_F24                313
+#define VENGINE_KEY_F25                314
+#define VENGINE_KEY_KP_0               320
+#define VENGINE_KEY_KP_1               321
+#define VENGINE_KEY_KP_2               322
+#define VENGINE_KEY_KP_3               323
+#define VENGINE_KEY_KP_4               324
+#define VENGINE_KEY_KP_5               325
+#define VENGINE_KEY_KP_6               326
+#define VENGINE_KEY_KP_7               327
+#define VENGINE_KEY_KP_8               328
+#define VENGINE_KEY_KP_9               329
+#define VENGINE_KEY_KP_DECIMAL         330
+#define VENGINE_KEY_KP_DIVIDE          331
+#define VENGINE_KEY_KP_MULTIPLY        332
+#define VENGINE_KEY_KP_SUBTRACT        333
+#define VENGINE_KEY_KP_ADD             334
+#define VENGINE_KEY_KP_ENTER           335
+#define VENGINE_KEY_KP_EQUAL           336
+#define VENGINE_KEY_LEFT_SHIFT         340
+#define VENGINE_KEY_LEFT_CONTROL       341
+#define VENGINE_KEY_LEFT_ALT           342
+#define VENGINE_KEY_LEFT_SUPER         343
+#define VENGINE_KEY_RIGHT_SHIFT        344
+#define VENGINE_KEY_RIGHT_CONTROL      345
+#define VENGINE_KEY_RIGHT_ALT          346
+#define VENGINE_KEY_RIGHT_SUPER        347
+#define VENGINE_KEY_MENU               348
