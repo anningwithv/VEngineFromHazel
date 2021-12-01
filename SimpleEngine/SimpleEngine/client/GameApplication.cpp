902c562e541432a6859383140133c07febf4464d
zwei <zwei@wooduan.com>
2021-12-01 11:01:28 +0800

    Transform

--- SimpleEngine/SimpleEngine/client/GameApplication.cpp
+++ SimpleEngine/SimpleEngine/client/GameApplication.cpp
@@ -43,6 +43,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -51,7 +52,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
 
--- SimpleEngine/SimpleEngine/src/VEngine/Renderer/Renderer.cpp
+++ SimpleEngine/SimpleEngine/src/VEngine/Renderer/Renderer.cpp
@@ -12,10 +12,12 @@ namespace VEngine
 	{
 	}
 
-	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray)
+	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray,
+		const glm::mat4& transform)
 	{
 		shader->Bind();
 		shader->UploadUniformMat4("u_ViewProjection", s_SceneData->ViewProjectionMatrix);
+		shader->UploadUniformMat4("u_Transform", transform);
 
 		vertexArray->Bind();
 		RendererCommand::DrawIndexed(vertexArray);
