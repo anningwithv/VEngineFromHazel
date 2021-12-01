9d35af96e94a6b7127a16d9ee164a38ca8363add
zwei <zwei@wooduan.com>
2021-12-01 17:45:51 +0800

    LayerStack bug fix

--- SimpleEngine/SimpleEngine/src/VEngine/Layer/LayerStack.cpp
+++ SimpleEngine/SimpleEngine/src/VEngine/Layer/LayerStack.cpp
@@ -5,7 +5,6 @@ namespace VEngine
 {
 	LayerStack::LayerStack()
 	{
-		m_LayerInsert = m_Layers.begin();
 	}
 
 	LayerStack::~LayerStack()
@@ -16,8 +15,8 @@ namespace VEngine
 
 	void LayerStack::PushLayer(Layer* layer) 
 	{
-		m_LayerInsert = m_Layers.emplace(m_LayerInsert, layer);
-		//m_Layers.emplace(m_Layers.begin() + m_LayerInsert, layer);
+		m_Layers.emplace(m_Layers.begin() + m_LayerInsertIndex, layer);
+		m_LayerInsertIndex++;
 		layer->OnAttach();
 	}
 
@@ -29,12 +28,12 @@ namespace VEngine
 
 	void LayerStack::PopLayer(Layer* layer) 
 	{
-		auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
-		if (it != m_Layers.end())
+		auto it = std::find(m_Layers.begin(), m_Layers.begin() + m_LayerInsertIndex, layer);
+		if (it != m_Layers.begin() + m_LayerInsertIndex)
 		{
 			layer->OnDetach();
 			m_Layers.erase(it);
-			m_LayerInsert--;
+			m_LayerInsertIndex--;
 		}
 	}
 
