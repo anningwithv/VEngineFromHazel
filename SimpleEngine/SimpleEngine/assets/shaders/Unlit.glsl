277ad47b07cd076081076d6f38b78939d32c4880
anningwithv <378294113@qq.com>
2021-12-19 12:53:46 +0800

    Depth Color

--- SimpleEngine/SimpleEngine/assets/shaders/Unlit.glsl
+++ SimpleEngine/SimpleEngine/assets/shaders/Unlit.glsl
@@ -71,7 +71,18 @@ struct PointLight
 #define NR_POINT_LIGHTS 1  
 uniform PointLight pointLights[NR_POINT_LIGHTS];
 
+float near = 0.1; 
+float far  = 100.0; 
+
+float LinearizeDepth(float depth) 
+{
+    float z = depth * 2.0 - 1.0; // back to NDC 
+    return (2.0 * near * far) / (far + near - z * (far - near));    
+}
+
 void main()
 {
-	color = texture(material.diffuse, v_TexCoord);
+    float depth = LinearizeDepth(gl_FragCoord.z) / far; // 为了演示除以 far
+    color = vec4(vec3(depth), 1.0);
+	//color = texture(material.diffuse, v_TexCoord);
 }
